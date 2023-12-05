e4d795d2d714a3f6e06e7248da35200fddf36334
wake <kakoy-to_wuk@mail.ru>
2023-12-05 03:26:42 +0300

    2 task hw7

---
+++ src/main/java/edu/hw7/Task2/ParallelFactorial.java
@@ -0,0 +1,13 @@
+package edu.hw7.Task2;
+
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+public class ParallelFactorial {
+
+    public int getFactorial(int n){
+        AtomicInteger counter = new AtomicInteger();
+        return Stream.generate(counter::incrementAndGet).limit(n).parallel().reduce(1, (l, m) -> l * m);
+    }
+}
