d93bdf1f1de26ea2f57acfd006f4de0a52570987
wake <kakoy-to_wuk@mail.ru>
2023-12-10 21:25:33 +0300

    add test project4

--- src/test/java/edu/project4/downloading/ImageUtilsTest.java
+++ src/test/java/edu/project4/downloading/ImageUtilsTest.java
@@ -5,7 +5,9 @@ import edu.project4.transformations.CylinderTransformation;
 import edu.project4.transformations.DiamondTransformation;
 import edu.project4.transformations.DiscTransformation;
 import edu.project4.transformations.FisheyeTransformation;
+import edu.project4.transformations.HandkerchiefTransformation;
 import edu.project4.transformations.HeartTransformation;
+import edu.project4.transformations.HorseshoeTransformation;
 import edu.project4.transformations.HyperbolicTransformation;
 import edu.project4.transformations.PolarTransformation;
 import edu.project4.transformations.SphericalTransformation;
@@ -13,6 +15,7 @@ import edu.project4.transformations.SpiralTransformation;
 import edu.project4.models.Rect;
 import edu.project4.renderers.OneThreadRenderer;
 import edu.project4.renderers.Renderer;
+import edu.project4.transformations.SwirlTransformation;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
@@ -64,6 +67,19 @@ class ImageUtilsTest {
             ), 4000, a
         );
         ImageUtils.save(canvas, Path.of("src/main/resources/project4/pic3"), ImageFormat.PNG);
+    }    @Test void save4() {
+        Renderer renderer = new OneThreadRenderer();
+        short a = 100;
+        var canvas = renderer.render(
+
+            1000, 1000, new Rect(-1, 1, -1, 1), List.of(
+
+                new HandkerchiefTransformation(),
+                new SwirlTransformation(),
+                new HorseshoeTransformation()
+            ), 4000, a
+        );
+        ImageUtils.save(canvas, Path.of("src/main/resources/project4/pic4"), ImageFormat.PNG);
     }
 
     @Test void save3MultiThread() {
